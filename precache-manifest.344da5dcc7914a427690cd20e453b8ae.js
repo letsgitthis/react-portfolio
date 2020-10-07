fd85b061594e71f5197e661dd59cf9939cff3f94
letsgitthis <jbentley1011@gmail.com>
2020-10-07 17:57:33 -0500

    Updates

---
+++ precache-manifest.344da5dcc7914a427690cd20e453b8ae.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5394de409cf241435b04023d5479aa7a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3da0f76b018914710e63",
+    "url": "/react-portfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "fee44809ff68208e4c64",
+    "url": "/react-portfolio/static/css/main.efe43438.chunk.css"
+  },
+  {
+    "revision": "3da0f76b018914710e63",
+    "url": "/react-portfolio/static/js/2.6237193b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.6237193b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee44809ff68208e4c64",
+    "url": "/react-portfolio/static/js/main.81127fc8.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "9248b4a7a7bf465c5d1120b29e9820ce",
+    "url": "/react-portfolio/static/media/Password Generator.9248b4a7.gif"
+  },
+  {
+    "revision": "9f295f8341fecc35db04d3e1575e5365",
+    "url": "/react-portfolio/static/media/Quiz.9f295f83.gif"
+  },
+  {
+    "revision": "d89f543edefdad9de491b8f36fd641ce",
+    "url": "/react-portfolio/static/media/Rock Paper Scissors.d89f543e.gif"
+  },
+  {
+    "revision": "6a237cd41f0c6f28e629cee88ba7392e",
+    "url": "/react-portfolio/static/media/Space Portal _ Main.6a237cd4.gif"
+  },
+  {
+    "revision": "b1b222ad6ac04f522cd4e727bcf9809a",
+    "url": "/react-portfolio/static/media/Weather Dashboard.b1b222ad.gif"
+  },
+  {
+    "revision": "9e382ae427c6ed6deba070cdc36fbc93",
+    "url": "/react-portfolio/static/media/Work Day Scheduler.9e382ae4.gif"
+  },
+  {
+    "revision": "4290a2f64a29d2465417be3914f41338",
+    "url": "/react-portfolio/static/media/background-image.4290a2f6.jpeg"
+  },
+  {
+    "revision": "b08e4e2988290a12f1736129633c920f",
+    "url": "/react-portfolio/static/media/profile-image.b08e4e29.jpg"
+  }
+]);
